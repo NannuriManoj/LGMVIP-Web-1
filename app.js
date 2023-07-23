3746e1117e47dfd8d423f5b2609365ecbf24d99f
NannuriManoj <106346544+NannuriManoj@users.noreply.github.com>
2023-07-23 22:54:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+document.getElementById('addButton').addEventListener('click', function (e) {
+    e.preventDefault();
+    const value = document.getElementById('todoInput').value;
+    if (value) {
+        addItem(value);
+        document.getElementById('todoInput').value = '';
+    }
+    else {
+        alert("Enter something");
+    }
+});
+
+function addItem(text) {
+    const list = document.getElementById('todoList');
+
+    const item = document.createElement('li');
+    item.className = 'todoItem';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', function () {
+        item.style.textDecoration = this.checked ? 'line-through' : 'none';
+    });
+
+    const taskText = document.createElement('span');
+    taskText.innerText = text;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.className = 'deleteButton';
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(item);
+    });
+
+    const newDiv = document.createElement("div");
+    newDiv.id = 'newDiv';
+    newDiv.appendChild(deleteButton);
+
+    item.appendChild(checkbox);
+    item.appendChild(taskText);
+    item.appendChild(newDiv);
+    list.appendChild(item);
+}
+
+
+
+
+
+
